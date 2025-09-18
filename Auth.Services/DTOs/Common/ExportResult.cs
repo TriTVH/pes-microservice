2b8bcdeff31bb5633a04f51604193d30eabc163c
Minh Tuấn <mtpham0246@gmail.com>
2025-09-18 11:20:12 +0700

    Teacher

---
+++ Auth.Services/DTOs/Common/ExportResult.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth.Application.DTOs.Common
+{
+    public record ExportResult(byte[] FileContents, string FileName);
+}
---
+++ Auth.Services/DTOs/UpdateProfileDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth.Application.DTOs
+{
+    public record UpdateProfileDto(
+    string? Name,
+    string? Phone,
+    string? Address,
+    string? AvatarUrl,
+    string? Gender
+);
+}
