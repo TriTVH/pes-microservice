1e794e2e4efa85d0cefa6073bafea1c6f7f1e315
TriTVH <161578251+TriTVH@users.noreply.github.com>
2025-09-26 21:39:56 +0700

    Update identity

--- Auth.Services/DTOs/Teacher/UpdateTeacherDto.cs
+++ Auth.Services/DTOs/Teacher/UpdateTeacherDto.cs
@@ -11,6 +11,7 @@ namespace Auth.Application.DTOs.Teacher
     string? Phone,
     string? Address,
     string? AvatarUrl,
-    string? Gender
+    string? Gender,
+    string? IdentityNumber
 );
 }
